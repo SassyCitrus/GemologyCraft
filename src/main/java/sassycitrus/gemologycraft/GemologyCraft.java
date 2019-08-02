aba8d81336a11c4b1c672ff22fe1a6132beba26b
Tryton Van Meer <trytonvanmeer@gmail.com>
2019-08-02 03:09:21 -0400

    Changed ItemGroup name.

--- src/main/java/sassycitrus/gemologycraft/GemologyCraft.java
+++ src/main/java/sassycitrus/gemologycraft/GemologyCraft.java
@@ -1,77 +1,23 @@
 package sassycitrus.gemologycraft;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import net.minecraft.block.Block;
-import net.minecraft.item.Item;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraft.util.Identifier;
 import sassycitrus.gemologycraft.init.GemologyBlocks;
 import sassycitrus.gemologycraft.init.GemologyItems;
 
-@Mod(GemologyCraft.MODID)
-public class GemologyCraft
+public class GemologyCraft implements ModInitializer
 {
     public static final String MODID = "gemologycraft";
-    public static final Logger LOGGER = LogManager.getLogger();
+    public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier(MODID, "creativetab"), () -> new ItemStack(GemologyItems.GEM_RUBY));
 
-    public static final ItemGroup ItemGroupGemology = new ItemGroup(MODID)
+    @Override
+    public void onInitialize()
     {
-    
-        @Override
-        public ItemStack createIcon()
-        {
-            return new ItemStack(GemologyItems.GEM_RUBY);
-        }
-    };
-
-    public GemologyCraft()
-    {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
-
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    private void setup(final FMLCommonSetupEvent event)
-    {
-
-    }
-
-    private void clientSetup(final FMLClientSetupEvent event)
-    {
-
-    }
-
-    private void enqueueIMC(final InterModEnqueueEvent event)
-    {
-        
-    }
-
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryHandler
-    {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> event)
-        {
-            GemologyBlocks.register(event.getRegistry());
-        }
-
-        @SubscribeEvent
-        public static void onItemsRegistry(final RegistryEvent.Register<Item> event)
-        {
-            GemologyItems.register(event.getRegistry());
-            GemologyBlocks.registerItemBlocks(event.getRegistry());
-        }
+        GemologyItems.register();
+        GemologyBlocks.register();
+        GemologyBlocks.registerItemBlocks();
     }
 }
